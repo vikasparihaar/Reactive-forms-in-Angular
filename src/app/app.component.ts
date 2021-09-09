829e37f59fa0bce89c45034244f1464580b100e3
KUMAR Vikas <vikasparihar864@gmail.com>
2021-09-09 07:41:47 +0530

    tried with nested form groups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  loginForm = new FormGroup({
+    user:new FormControl('', [Validators.required,Validators.pattern('[a-zA-Z]+$')]),
+    userEmail:new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required,Validators.minLength(8)]),
+    
+    address :new FormGroup({
+      city:new FormControl('',[Validators.required]),
+      state:new FormControl('',[Validators.required]),
+      postalCode: new FormControl('',[Validators.required,Validators.minLength(6)])
+
+    })
+    
+  });
+  loginUser(){
+    console.warn(this.loginForm.value)
+  }
+  get user(){
+    return this.loginForm.get('user')
+  }
+  get userEmail(){
+    return this.loginForm.get('userEmail')
+  }
+  get password(){
+    return this.loginForm.get('password')
+  }
+  
+  get city(){
+    return this.loginForm.get('city')
+  }
+  get state(){
+    return this.loginForm.get('sate')
+  }
+  get postalCode(){
+    return this.loginForm.get('postalCode')
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
